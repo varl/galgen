e34361ec6053958148a1171c68f6400d8ecf3935
Viktor Varland <viktor@dhis2.org>
2018-01-01 17:50:38 +0100

    Update stuff

--- galgen.py
+++ galgen.py
@@ -2,11 +2,17 @@
 
 import argparse
 import os
+import datetime
 
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+from lib import html_tmpl
+
 def exif(info):
+    if info is None:
+        return {}
+
     myTags = ['ISOSpeedRatings', 'Model', 'LensModel', 'FocalLength',
             'DateTime', 'ExposureTime', 'Orientation']
     ret = {}
@@ -26,97 +32,8 @@ def is_image(i):
     else:
         return False
 
-def build_list(elements):
-    htmlList = []
-    htmlList.append('<ul>')
-    for el in elements:
-        htmlList.append('<li>')
-        htmlList.append('<div>')
-        htmlList.append('<a href="'+el[0]+'"><img src="'+el[1]+'"></a>')
-        htmlList.append('<div>')
-        htmlList += build_exif_list(el[2])
-        htmlList.append('</div>')
-        htmlList.append('</div>')
-        htmlList.append('</li>')
-    htmlList.append('</ul>')
-    return htmlList
-
-def build_exif_list(exif):
-    exifList = []
-    
-    exifList.append('<dl>')
-    for k, v in exif.items():
-        exifList.append('<dt>')
-        exifList.append('{}'.format(k))
-        exifList.append('</dt>')
-        exifList.append('<dd>')
-        exifList.append('{}'.format(v))
-        exifList.append('</dd>')
-
-    exifList.append('</dl>')
-    return exifList
-
-def build_doc(path, images):
-    doc = []
-    doc.append('<!doctype html>')
-    doc.append('<meta charset="utf-8">')
-    doc.append('<meta name="viewport" content="width=device-width,initial-scale=1">')
-    doc.append('<title></title>')
-    doc += build_css()
-    doc.append('<article>')
-    doc += build_list(images)
-    doc.append('</article>')
-    doc.append('<script type="text/javascript" src="main.js"></script>')
-    return ''.join(doc)
-
-def build_css():
-    css = []
-    css.append('<style>')
-    css.append('body { background-color: #181818; }')
-    css.append(('ul { list-style:none;'
-                'display: flex;'
-                'flex-direction:row;'
-                'flex-wrap: wrap;'
-                'padding: 0;'
-                'margin: 0;'
-                'justify-content: center;'
-                'align-content: space-around;'
-                '}'))
-    css.append(('li {'
-                'margin: 0.2em;'
-                'border-collapse: collapse;'
-                '}'))
-    css.append(('li > div {'
-                'display: flex;'
-                'flex-direction: row;'
-                'flex-wrap: wrap;'
-                '}'))
-    css.append(('li > div > div {'
-                'padding: 1em;'
-                'color: #efefef;'
-                'font-family: monospace;'
-                'font-size: 16px;'
-                '}'))
-    css.append('dt { font-weight: 600; color: rebeccapurple; }')
-    css.append('a { border: 0.2em solid orange; display: inline-flex; }')
-    css.append('a:hover { border: 0.2em solid rebeccapurple; }')
-    css.append('img { max-width: 100%; max-height: 100%; }')
-    css.append('</style>')
-    return css
-
-if __name__ == '__main__':
+def thumber(directory, gallery_dir):
     THUMB_SIZE = (1152, 864)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory', 
-            help='directory to turn into a site')
-
-    args = parser.parse_args()
-    
-    if args.directory:
-        directory = args.directory
-    else:
-        directory = os.getcwd()
 
     d = os.scandir(directory)
     images = [(i.name, i.path) for i in d if is_image(i)]
@@ -128,21 +45,49 @@ if __name__ == '__main__':
         thumb = file + '_thumb.jpg'
 
         im = Image.open(image[1])
+
+        im_cp = im.copy()
+        im_cp.save(os.path.join(gallery_dir, file+'.jpg'), "JPEG")
+
         info = exif(im._getexif())
-        path = os.path.join(directory, thumb)
+        path = os.path.join(gallery_dir, thumb)
 
         im.thumbnail(THUMB_SIZE, Image.ANTIALIAS)
         o = info.get('Orientation', -1)
         if o == 8:
-            print('[FLIP] Rotate 90 deg cw.')
+            log('flip', 'Rotate 90 deg cw.')
             im = im.rotate(90)
-        im.save(path, "JPEG")
-        thumbed.append((image[0], thumb, info))
-        print('[DONE] Created thumbnail for "{}"..'.format(file))
 
-    with open(os.path.join(directory, 'index.html'), 'w') as index:
-        print('Write the document')
-        index.write(build_doc(directory, thumbed))
+        im.save(path, "JPEG")
+
+        thumbed.append((image[0], thumb, info))
+        log('thumb', 'Created thumbnail for "{}"..'.format(file))
+
+        return thumbed
+ 
+def log(label='LOG', msg=''):
+    ts = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    print('['+ts+'] ['+label.upper()+'] ' + msg)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', 
+            help='directory to turn into a site')
+
+    args = parser.parse_args()
     
-    print('=======================')
-    print('... Done!')
+    if args.directory:
+        directory = args.directory
+    else:
+        directory = os.getcwd()
+
+    gallery_dir = os.path.join(directory, 'gal')
+    os.makedirs(gallery_dir, exist_ok=True)
+
+    thumbed = thumber(directory, gallery_dir)
+
+    with open(os.path.join(gallery_dir, 'index.html'), 'w') as index:
+        log('shit', 'Write the document')
+        index.write(html_tmpl.build_doc(directory, thumbed))
+
+    log('SUCCESS', '... Done!')
